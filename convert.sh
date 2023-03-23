9b3acaf245651a5106cb3e4d2c2c24f0db028258
Peter Hellberg <peter@c7.se>
2023-03-23 20:22:10 +0100

    Convert &amp; to & using sed

--- convert.sh
+++ convert.sh
@@ -1,2 +1,2 @@
 #!/bin/env sh
-/usr/bin/mlr --icsv --ojson sort -n index data/companies.csv | jq . > data/companies.json
+/usr/bin/mlr --icsv --ojson sort -n index data/companies.csv | sed "s/\&amp;/\&/g" | jq . > data/companies.json
